e92733102a218c6b763931294eb143e93ca97991
aishaimtiyaz <144771688+aishaimtiyaz@users.noreply.github.com>
2023-12-16 23:38:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function getFormvalue() {
-    //Write your code here
-
-}
+function getFormValue() {
+        // Corrected method name to getElementsByName
+        let fname = document.getElementsByName("fname")[0].value;
+        let lname = document.getElementsByName("lname")[0].value;
+        alert(fname + " " + lname);
+    }
